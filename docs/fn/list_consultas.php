87950fe18b82004d73bae63e3c3b214de378cfdb
cesarmario <cesarmariosdelosrios@gmail.com>
2022-09-04 14:47:28 -0300

    datos cosnultas

--- docs/fn/list_consultas.php
+++ docs/fn/list_consultas.php
@@ -6,9 +6,9 @@
     $listadoConsultas .= "<thead>";
     $listadoConsultas .= "<tr>";
     $listadoConsultas .= "<th>Fecha</th>";
+    $listadoConsultas .= "<th>Nombre y Apellido</th>";
     $listadoConsultas .= "<th>Inmueble</th>";
     $listadoConsultas .= "<th>Agente</th>";
-    $listadoConsultas .= "<th>Nombre y Apellido</th>";
     $listadoConsultas .= "<th>Respuesta</th>";
     $listadoConsultas .= "<th></th>";
     $listadoConsultas .= "</tr>";
@@ -17,9 +17,9 @@
     while($consultas=mysqli_fetch_assoc($rtsconsultas)){
         $listadoConsultas .= "<tr>";
         $listadoConsultas .= "<td>". $consultas['fechaConsulta'] . "</td>";
+        $listadoConsultas .= "<td>". $consultas['nombreConsulta'] . "</td>";
         $listadoConsultas .= "<td>". $consultas['tituloInmueble'] . "</td>";
         $listadoConsultas .= "<td>". $consultas['nombreAgente'] . "</td>";
-        $listadoConsultas .= "<td>". $consultas['nombreConsulta'] . "</td>";
         $listadoConsultas .= "<td>". $consultas['fechaRespuestaConsulta'] . "</td>";
         $listadoConsultas .= "<td><a href='consulta_abm.php?idConsulta=". $consultas['idConsulta'] ."' class='btn btn-warning me-1 mb-1'>Editar</a></td>";
         $listadoConsultas .= "</tr>";

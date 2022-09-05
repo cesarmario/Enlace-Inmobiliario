d928a5b4bf34454fdb9ab4ea8638179f59ca9e84
cesarmario <38296420+cesarmario@users.noreply.github.com>
2022-09-05 09:45:24 -0300

    estadisticas Index

--- docs/fn/estaditicas.php
+++ docs/fn/estaditicas.php
@@ -1,14 +1,34 @@
 <?PHP
     include('conexion.php');
+    $totalInmuebles = 0;
     $queryInmuebles = "SELECT COUNT(`idInmueble`) AS totalInmuebles FROM inmueble WHERE baja != '1' GROUP BY baja";
     $rtsInmuebles = mysqli_query($conexion, $queryInmuebles);
-    $inmuebles=mysqli_fetch_assoc($rtsInmuebles);
-
+    if (mysqli_num_rows($rtsInmuebles)>0){
+        $inmuebles=mysqli_fetch_assoc($rtsInmuebles);
+        $totalInmuebles = $inmuebles['totalInmuebles'];
+    } else { 
+        $totalInmuebles = '0';
+    }    
+    
     $queryConsultas = "SELECT COUNT(`idConsulta`) AS totalConsultas FROM consulta WHERE baja != '1' GROUP BY baja";
     $rtsConsultas = mysqli_query($conexion, $queryConsultas);
-    $consultas=mysqli_fetch_assoc($rtsConsultas);
+    if (mysqli_num_rows($rtsConsultas)>0){
+        $consultas=mysqli_fetch_assoc($rtsConsultas);
+        $totalConsultas = $consultas['totalConsultas'];
+    } else { 
+        $totalConsultas = '0';
+    }  
 
+
+    ;
     $queryPedidos = "SELECT COUNT(`idPedido`) AS totalPedidos FROM pedido WHERE baja != '1' GROUP BY baja";
     $rtsPedidos = mysqli_query($conexion, $queryPedidos);
-    $pedidos=mysqli_fetch_assoc($rtsPedidos)
+    if (mysqli_num_rows($rtsPedidos)>0){
+        $pedidos=mysqli_fetch_assoc($rtsPedidos);        
+        $totalPedidos = $pedidos['totalPedidos'];
+    } else { 
+        $totalPedidos = '0';
+    }  
+
+
 ?>
--- docs/index.php
+++ docs/index.php
@@ -147,7 +147,7 @@
                                             </div>
                                             <div class="col-md-8">
                                                 <h6 class="text-muted font-semibold">Inmuebles</h6>
-                                                <h6 class="font-extrabold mb-0"><?PHP echo $inmuebles['totalInmuebles']; ?></h6>
+                                                <h6 class="font-extrabold mb-0"><?PHP echo $totalInmuebles; ?></h6>
                                             </div>
                                         </div>
                                     </div>
@@ -164,7 +164,7 @@
                                             </div>
                                             <div class="col-md-8">
                                                 <h6 class="text-muted font-semibold">Consultas</h6>
-                                                <h6 class="font-extrabold mb-0"><?PHP echo $consultas['totalConsultas']; ?></h6>
+                                                <h6 class="font-extrabold mb-0"><?PHP echo $totalConsultas; ?></h6>
                                             </div>
                                         </div>
                                     </div>
@@ -181,7 +181,7 @@
                                             </div>
                                             <div class="col-md-8">
                                                 <h6 class="text-muted font-semibold">Pedidos</h6>
-                                                <h6 class="font-extrabold mb-0"><?PHP echo $pedidos['totalPedidos']; ?></h6>
+                                                <h6 class="font-extrabold mb-0"><?PHP echo $totalPedidos; ?></h6>
                                             </div>
                                         </div>
                                     </div>

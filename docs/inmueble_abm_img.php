bc29cae1917e85d9c7f80c2208e459a957c2e693
cesarmario <cesarmariosdelosrios@gmail.com>
2022-09-13 22:45:34 -0300

    Update inmueble_abm_img.php

--- docs/inmueble_abm_img.php
+++ docs/inmueble_abm_img.php
@@ -421,6 +421,8 @@
         })
     });
 </script>
+    <script src="assets/vendors/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
     <script src="https://kit.fontawesome.com/1ffc2bde27.js" crossorigin="anonymous"></script>
     <script src="assets/js/main.js"></script>
 </body>

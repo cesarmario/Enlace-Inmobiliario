5dbe170557db9eb3e83aa375d34c64dcf7e09fcc
cesarmario <cesarmariosdelosrios@gmail.com>
2022-09-17 17:41:57 -0300

    Update map.js

--- docs/assets/js/map.js
+++ docs/assets/js/map.js
@@ -1,7 +1,35 @@
 const tilesProvider = "	https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 
-let myMap = L.map('myMap').setView([-31.5358, -68.5458], 14)
+let myMap = L.map('myMap').setView([-31.5373, -68.5251], 14)
 
 L.tileLayer(tilesProvider, {
  maxZoom: 18,   
-}).addTo(myMap);
+}).addTo(myMap)
+
+let marker = L.marker([-31.5373, -68.5251]).addTo(myMap)
+
+myMap.doubleClickZoom.disable()
+
+myMap.on('dblclick', e => {
+    let latLng = myMap.mouseEventToLatLng(e.originalEvent)
+    console.log(latLng)
+    L.marker([latLng.lat, latLng.lng]).addTo(myMap)
+})
+
+/*
+navigator.geolocation.getCurrentPosition(
+    (pos) => {
+        const {coords} = pos
+        console.log(coords)
+        L.marker([coords.latitude, coords.longitude]).addTo(myMap)
+    },
+    (err) => {
+        console.log(err)
+    },
+    {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0
+    }
+    
+) */

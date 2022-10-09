3e1a9618a41431812ef8f840ead41dec00d5e87f
cesarmario <cesarmariosdelosrios@gmail.com>
2022-10-09 10:52:51 -0300

    mapa test

---
+++ docs/assets/js/mapview.js
@@ -0,0 +1,47 @@
+const tilesProvider = "	https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+//const input = document.querySelector('input');
+//const log = document.getElementById('coordenadas');
+
+let myMap = L.map('myMap').setView([-31.5373, -68.5251], 14)
+
+L.tileLayer(tilesProvider, {
+ maxZoom: 18,   
+}).addTo(myMap)
+
+var jsvar = '<?PHP echo $latLng?>';
+console.log(jsvar); 
+
+document.getElementById('coord').addEventListener('onLoad',function(e){
+    //let coords = e.target.value.split(",");
+    L.marker([coords.lat, coords.lng]).addTo(myMap)
+    map.flyTo(coords,13);
+    console.log(latLng)
+  })
+
+//let marker = L.marker([-31.5373, -68.5251]).addTo(myMap)
+
+myMap.doubleClickZoom.disable()
+
+/*myMap.on('dblclick', e => {
+    let latLng = myMap.mouseEventToLatLng(e.originalEvent)
+    console.log(latLng)
+    L.marker([latLng.lat, latLng.lng]).addTo(myMap)
+    document.getElementById("coordenadas").value = latLng;
+}) */
+
+/*
+    navigator.geolocation.getCurrentPosition(
+    (pos) => {
+        const {coords} = pos
+        console.log(coords)
+        L.marker([coords.latitude, coords.longitude]).addTo(myMap)
+    },
+    (err) => {
+        console.log(err)
+    },
+    {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0
+    }    
+) */
